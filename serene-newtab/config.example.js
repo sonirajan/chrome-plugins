de66afb646033ef051f549da6ae48215ce0aa81f
Rajan Soni <rajan.soni@yahoo.com>
2026-05-06 10:54:16 -0700

    chore: comment fixed

--- serene-newtab/config.example.js
+++ serene-newtab/config.example.js
@@ -1,4 +1,4 @@
-// Before loading the extension, copy `config.example.js` to `config.js`
+// To use Unsplash images, copy `config.example.js` to `config.js`
 // Add your Unsplash access key generated from https://unsplash.com/developers
 const CONFIG = {
     ACCESS_KEY: 'your_unsplash_access_key_here',
