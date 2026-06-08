18a1e52f1c409884033be6b72af5c9a935ccf294
Rajan Soni <rajan.soni@yahoo.com>
2026-06-08 12:18:40 -0700

    more quotes and margin fix

--- serene-newtab/quotes.js
+++ serene-newtab/quotes.js
@@ -28,6 +28,8 @@ const QUOTES = [
     "Doing nothing is far better than doom scrolling.",
     "No doom scrolling. No junk food. Daily exercise. Deep sleep. Daily meditation.",
     "Take baby steps — small enough to accomplish, big enough to feel the progress and keep going.",
+    "Is the current task valuable enough to cancel other work ? - If No, it is garbage work!",
+    "Lots of things are urgent. But urgency and priority are absolutely not the same thing. Your default should be to do only your highest priority work. And then you can consider the urgency of that work to consider slightly changing the order you work on things.",
 
     // Gujarati
     "યોગઃ કર્મશુ કૌશલમ! — કર્મમાં કુશળતા એ જ યોગ છે.",
